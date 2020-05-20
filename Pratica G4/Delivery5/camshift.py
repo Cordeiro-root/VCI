e78b02131b04b7ce2de9affcdeb97dd077119496
joao <joao.cordeiro@ua.pt>
2020-05-20 14:20:20 +0100

    works, but not as we want

--- Pratica G4/Delivery5/camshift.py
+++ Pratica G4/Delivery5/camshift.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv
 
 cap = cv.VideoCapture('video.mp4')
+
 # take first frame of the video
 ret,frame = cap.read()
 
@@ -18,21 +19,22 @@ cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
 
 # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
 term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
-
 while(1):
     ret, frame = cap.read()
     if ret == True:
         frame = cv.resize(frame, (1080, 700))
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        #cv.imshow('hsv',hsv)
         dst = cv.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-        # apply meanshift to get the new location
-        ret, track_window = cv.meanShift(dst, track_window, term_crit)
-        print(track_window)
+
+        # apply camshift to get the new location
+        ret, track_window = cv.CamShift(dst, track_window, term_crit)
+
         # Draw it on image
-        x,y,w,h = track_window
-        img2 = cv.rectangle(frame, (x,y), (x+w,y+h), 255,2)
+        pts = cv.boxPoints(ret)
+        pts = np.int0(pts)
+        img2 = cv.polylines(frame,[pts],True, 255,2)
         cv.imshow('img2',img2)
+        
         k = cv.waitKey(30) & 0xff
         if k == 27:
             break
